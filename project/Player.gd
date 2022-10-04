23d39ccbfad0c5c511fa4e9dde719e26c8469ff0
Daniel <djisenberg2015@gmail.com>
2022-10-04 14:44:09 -0400

    Add laser eyes to player object

--- project/Player.gd
+++ project/Player.gd
@@ -4,8 +4,20 @@ var _velocity := Vector2.ZERO
 var _speed := 150
 var _gravity := 600
 var _jump_impulse := 300
+var _laser = preload("res://Laser.tscn")
 
 onready var _sprite := find_node("AnimatedSprite")
+
+func _process(_delta):
+	if Input.is_action_pressed("laser"):
+		var _laserEyes0 = _laser.instance()
+		var _laserEyes1 = _laser.instance()
+		_laserEyes0.move_local_x(18)
+		_laserEyes0.move_local_y(-22)
+		_laserEyes1.move_local_x(50)
+		_laserEyes1.move_local_y(-22)
+		add_child(_laserEyes0)
+		add_child(_laserEyes1)
 
 func _ready():
 	pass
@@ -26,8 +38,7 @@ func _physics_process(delta):
 		_velocity.y = _velocity.y + _gravity * delta
 		
 	
-# warning-ignore:return_value_discarded	
-	move_and_slide(_velocity, Vector2.UP)
+	_velocity = move_and_slide(_velocity, Vector2.UP)
 	
 	
 	if not _velocity.x == 0:
